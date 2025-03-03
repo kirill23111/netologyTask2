60218155fdc54d9d207f3a06d05fc55dbd5f3b9f
kirill23111 <110820421+kirill23111@users.noreply.github.com>
2025-03-03 21:17:55 +0100

    Delete task2/src directory

--- task2/src/ru/netology/service/CustomsService.java
+++
@@ -1,8 +0,0 @@
-package ru.netology.service;
-
-public class CustomsService {
-    public static int calc(int price, int weight) {
-        int result  = (price / 100) + (weight * 100);
-        return result;
-    }
-}
